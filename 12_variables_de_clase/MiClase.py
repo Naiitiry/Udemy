78cdf067e478986a8e0b4fbce37affc5f3a11f10
Naiitiry <danchukroman@gmail.com>
2024-05-28 10:00:30 -0300

    clase 130

--- 12_variables_de_clase/MiClase.py
+++ 12_variables_de_clase/MiClase.py
@@ -6,12 +6,21 @@ class MiClase:
         self.variable_instancia = variable_instancia
 
     #clase 128 - Metodo estático
-
     @staticmethod
     #No puede acceder al atributo o variable de instancia.
     #Pero si a la de clase, pero no directamente.
     def metodo_estatico():
         print(MiClase.variable_clase)
+    
+    # Clase 129 - Método de clase
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+
+    # Clase 130 - contexto dinámico y estático
+    def metodo_instacia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
 
 '''
 print(MiClase.variable_clase) #Directamente de la clase.
@@ -22,6 +31,10 @@ print(miclase.variable_clase) # también se puede acceder desde el objeto.
 # clase 127
 # Se puede crear variables de clase al vuelo (nuevas variables)
 
-MiClase.variable_clase2 = 'Nuevo valor variable clase 2'
+#MiClase.variable_clase2 = 'Nuevo valor variable clase 2'
 
-MiClase.metodo_estatico() #Accedemos al método estático
+# MiClase.metodo_estatico() #Accedemos al método estático
+
+# MiClase.metodo_clase()
+objeto1 = MiClase('Variable instancia')
+objeto1.metodo_instacia() # Ejecuta 2 veces porque el método tiene 2 métodos más.
